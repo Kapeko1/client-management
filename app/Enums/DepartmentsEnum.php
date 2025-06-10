4ed5d6278c02bea4b6021112ca36e41c815dbf5b
Kacper Gądek <kacper.gadek2@sheepcoders.pl>
2025-06-11 00:23:30 +0200

    feat: create enum for departments

---
+++ app/Enums/DepartmentsEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum DepartmentsEnum : string
+{
+    case Administration = "Administration";
+    case InformationTechnology = "Information Technology";
+    case Marketing = "Marketing";
+    case PublicRelations = "Public Relations";
+}
+
