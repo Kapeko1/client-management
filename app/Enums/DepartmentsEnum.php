4c3da06c496a5584360387665e03c26c398917f8
Kacper Gądek <kacper.gadek2@sheepcoders.pl>
2025-06-11 18:47:12 +0200

    feat: created labels for Departments names

--- app/Enums/DepartmentsEnum.php
+++ app/Enums/DepartmentsEnum.php
@@ -8,5 +8,18 @@ enum DepartmentsEnum : string
     case InformationTechnology = "Information Technology";
     case Marketing = "Marketing";
     case PublicRelations = "Public Relations";
+
+
+
+    public function label(): string
+    {
+        return match($this) {
+            self::Administration => 'Administracja',
+            self::InformationTechnology => 'IT',
+            self::Marketing => 'Marketing',
+            self::PublicRelations => 'PR',
+        };
+    }
+
 }
 
