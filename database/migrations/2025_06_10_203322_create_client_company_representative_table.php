2432186681f23cd695fa3315227b03a74fc45f73
Kacper Gądek <kacper.gadek2@sheepcoders.pl>
2025-06-11 16:08:10 +0200

    fix: removed timestamps from pivotal table client_company_representative migration

--- database/migrations/2025_06_10_203322_create_client_company_representative_table.php
+++ database/migrations/2025_06_10_203322_create_client_company_representative_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('client_id')->constrained()->onDelete('cascade');
             $table->foreignId('company_representative_id')->constrained()->onDelete('cascade');
-            $table->timestamps();
         });
     }
 
