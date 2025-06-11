ebea2b062caa0f50c224f048cc8fb9bb73049d1f
Kacper Gądek <kacper.gadek2@sheepcoders.pl>
2025-06-11 12:07:45 +0200

    feat: add routes for clients displaying and creation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ClientController::class, 'index'])->name('clients.index');
+
+
+Route::get('/clients/create', [ClientController::class, 'create'])->name('clients.create');
+Route::post('/clients', [ClientController::class, 'store'])->name('clients.store');
+
