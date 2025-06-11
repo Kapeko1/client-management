5ca1e0a1452201320366b29648456a159032d819
Kacper Gądek <kacper.gadek2@sheepcoders.pl>
2025-06-11 18:47:37 +0200

    fix: removed unused navigation  from layput

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,19 +8,11 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-gray-100 font-sans">
-<nav class="bg-white shadow-md">
-    <div class="container mx-auto px-6 py-3">
-        <div class="flex items-center justify-between">
-            <div>
-            </div>
-            <div>
-                <a href="" class="px-4 py-2 text-gray-600 hover:text-gray-800 rounded-md @if(request()->routeIs('clients.*')) font-bold text-blue-600 @endif">Zarządzaj Klientami</a>
-                <a href="" class="px-4 py-2 text-gray-600 hover:text-gray-800 rounded-md @if(request()->routeIs('data.*')) font-bold text-blue-600 @endif">Wyświetl Dane</a>
-            </div>
-        </div>
-    </div>
-</nav>
+<body class="bg-gray-200 font-sans">
+{{--
+<nav class="bg-white shadow-md"> <
+/nav
+--}}
 
 <main class="container mx-auto px-6 py-8">
     @yield('content')
